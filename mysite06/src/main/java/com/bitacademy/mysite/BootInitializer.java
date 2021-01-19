4cf52e9dc76843862cff008163ab72fd320553c1
집 <@DESKTOP-L00N2LG>
2021-01-19 11:01:02 +0900

    설정

--- mysite06/src/main/java/com/bitacademy/mysite/BootInitializer.java
+++ mysite06/src/main/java/com/bitacademy/mysite/BootInitializer.java
@@ -5,12 +5,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EnableAutoConfiguration
-public class BootIntiializer extends SpringBootServletInitializer {
-
+public class BootInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		// TODO Auto-generated method stub
 		return builder.sources(BootApp.class);
 	}
-
 }
